6102e5cf55d8c3441661685e2d0bdb9690a5d1b2
Holk Neo <liaohaoxiang520@gmail.com>
2020-07-29 15:29:02 +0800

    [feat] create sitemap.xml & robots.txt, push seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import Head from 'next/head';
 //引用highlight样式
 // import 'highlight.js/styles/dracula.css'
 import '../styles/tailwind.css'
 import '../styles/markdown.css'
 import '../styles/drucula.css'
 
+export const siteTitle = "Neo's Blog";
+
 const App = ({ Component, pageProps }) => {
   return (
+    <>
+    <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="liaohaoxiang" />
+        <link rel="icon shortcut" href="/images/favicon.ico" type="image/x-icon"/>
+        <meta
+          name="description"
+          content="Blog Record Tech and Life"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
     <Component {...pageProps} />
+    </>
   )
 }
 
